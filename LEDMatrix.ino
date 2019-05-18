c284362bc0aaaeacfb25d100a5077499abf39088
Marco Uderzo <47079771+marcouderzo@users.noreply.github.com>
2019-05-18 15:08:49 +0200

    Fixed bugs

--- LEDMatrix.ino
+++ LEDMatrix.ino
@@ -6,28 +6,16 @@
 #define mHeight 15
 #define mWidth 20
 const int audioPin = A0; // Left or right channel audio positive lead connects here
-#define maxExpectedAudio 150   // Change this value to adjust the sensitivity
+#define maxExpectedAudio 120   // Change this value to adjust the sensitivity
 
 
 CRGB leds[NUM_LEDS];
-char data[20];  // FFT array variable
-char im[]={0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,}; //FFT im data
+char data[20];  // FFT array variables
+char im[]={0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,};
 
+uint16_t pixel;
 
-
-//**************************
-/*
-    @documentation: getPixel() takes x y values, calculates the LED position considering the matrix 
-    is a serpentine-like vector, and returns index used to identify LED in leds[] array. uint8_t is an unsigned 
-    8bit char type, used instead of int to save heap memory. This way SRAM is unlikely to get filled.
-
-    @optimization: possibly cleaner: if(y%2==0) y*mWidth+reverseX. if row(y)is even, with row(i)being i*mWidth. This way the 
-    XYTable[] array wouldn't be necessary as "i" could be used directly as the index of CRGB leds[] array.
-*/
-
-uint8_t getPixel(int x, int y) 
-{
-  uint8_t XYTable[]={
+uint16_t XYTable[]={
     0,   1,   2,   3,   4,   5,   6,   7,   8,   9,  10,  11,  12,  13,  14,  15,  16,  17,  18,  19,
     39,  38,  37,  36,  35,  34,  33,  32,  31,  30,  29,  28,  27,  26,  25,  24,  23,  22,  21,  20,
     40,  41,  42,  43,  44,  45,  46,  47,  48,  49,  50,  51,  52,  53,  54,  55,  56,  57,  58,  59,
@@ -45,8 +33,22 @@ uint8_t getPixel(int x, int y)
    280, 281, 282, 283, 284, 285, 286, 287, 288, 289, 290, 291, 292, 293, 294, 295, 296, 297, 298, 299
   };
 
-  uint8_t i = (y * mWidth) + x;
-  uint8_t j = XYTable[i];
+
+
+//**************************
+/*
+    @documentation: getPixel() takes x y values, calculates the LED position considering the matrix 
+    is a serpentine-like vector, and returns index used to identify LED in leds[] array. uint8_t is an unsigned 
+    8bit char type, used instead of int to save heap memory. This way SRAM is unlikely to get filled.
+
+    @optimization: possibly cleaner: if(y%2==0) y*mWidth+reverseX. if row(y)is even, with row(i)being i*mWidth. This way the 
+    XYTable[] array wouldn't be necessary as "i" could be used directly as the index of CRGB leds[] array.
+*/
+
+uint16_t getPixel(int x, int y) 
+{
+  uint16_t i = (y * mWidth) + x;
+  uint16_t j = XYTable[i];
   return j;
 }
 
@@ -54,9 +56,9 @@ uint8_t getPixel(int x, int y)
 /*
     @documentation: pushBar() draws the frequency columns and their current intensity according to the values processed by fix_fft()
 */
+
 void pushBar (int x, int y)
 {
-  uint8_t pixel;
   for (int i = 0; i<y; i++)
   {
     pixel=getPixel(x,i);
@@ -76,18 +78,23 @@ void setup()
 
 void loop() 
 { 
-  for (int i = 0; i < 20; i++) 
+  for (int i = 0; i < 40; i++) 
   {
     data[i] = analogRead(A5);
+    map(data[i],0,1023,0,maxExpectedAudio);
   }
 
- fix_fft(data, im, mWidth, 0);  
+  fix_fft(data, im, mWidth, 0);  
+
+  //ledarray(20,300)=CRGB(0,0,0); // use this instead on clear() if you want the first row not to visibly cycle 
   
-  for(int i=0; i<mWidth; i++)
+  FastLED.clear();
+  
+  for(int i=mWidth-1; i>=0; i--)
   {
-   // data[i] = sqrt(data[i]*data[i] + im[i]*im[i]);  // make values positive in case of negative values
-    data[i] = map(data[i],0,maxExpectedAudio,0,mHeight);
+    // data[i] = sqrt(data[i]*data[i] + im[i]*im[i]);
+    data[i] = map(data[i],0,maxExpectedAudio,1,mHeight);
     pushBar(i, data[i]);
   }
-  FastLED.clear();
+  delayMicroseconds(500); 
 }
