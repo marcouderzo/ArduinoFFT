61989b4b6b75f63940eb8fa3911a00550d97c041
Marco Uderzo <47079771+marcouderzo@users.noreply.github.com>
2019-05-22 21:20:14 +0200

    Downscaled and averaged data[] to fit matrix width

--- SpectrumAnalyzer.ino
+++ SpectrumAnalyzer.ino
@@ -1,6 +1,6 @@
 #include <FastLED.h>
 #include <LEDMatrix.h>
-#include <fix_fft.h>
+#include <fix_fft.h>   //to get input from audio source, connect audio output to Analog 0.
 
 #define mHeight 15
 #define mWidth 20
@@ -8,13 +8,10 @@
 #define mType HORIZONTAL_ZIGZAG_MATRIX
 #define DATA_PIN 12
 #define audioPin A5 // Left or right channel audio positive lead connects here
-#define maxExpectedAudio 120   // Change this value to adjust the sensitivity
+#define maxFreqAmplitude 40   // Change this value to adjust the sensitivity
 
-
-char data[128];  // FFT array variables
-char im[128];
+char data[128], im[128];  // FFT Array Variables
 char data_avgs[20];
-int j;
 
 cLEDMatrix < -mWidth, mHeight, mType > leds;
 
@@ -23,7 +20,6 @@ void setup()
   FastLED.addLeds<WS2812B, DATA_PIN, GRB>(leds[0], leds.Size());
   FastLED.setBrightness(70);
   //FastLED.clear(true);
-  //Serial.begin(9600);
 }
 
 void loop()
@@ -44,21 +40,23 @@ void loop()
 
   FastLED.clear();
 
-  for (int i = 0; i < 20; i++) // need to fix array downscaling, some frequencies are out of boundaries
+  for (int i = 0; i < 20; i++) //average values into a smaller array to fit the matrix width
   {
-    j = i << 0, 4;
-    data_avgs[i] = data[j] + data[j + 1] + data[j + 2] + data[j + 3] + data[j + 4] + data[j + 5] + data[j + 6] + data[j + 7] + data[j + 8] + data[j + 9] + data[j + 10] + data[j + 11] + data[j + 12] + data[j + 13] + data[j + 14] + data[j + 15] + data[j + 16] + data[j + 17] + data[j + 18] + data[j + 19];
-    if (i == 0) data_avgs[i] >>= 1;
+    data_avgs[i] = data[i * 3] + data[i * 3 + 1] + data[i * 3 + 2];
+    if (i == 0) data_avgs[i] >>= 1;  // de-emphasize first band, as it is too sensitive
   }
-  
+
+
   for (int i = 0; i < 20; i++)
   {
-    data_avgs[i] = map(data_avgs[i], 0, maxExpectedAudio, 0, mHeight);
+
+    data_avgs[i] = map(data_avgs[i], 0, maxFreqAmplitude, 0, mHeight);
+
   }
 
   for (int i = 0; i < mWidth; i++)
   {
-    leds.DrawLine(i, 0, i, data_avgs[i], CRGB(0, 255, 0));
+    leds.DrawLine(i, 0, i, data_avgs[i], CRGB(255 - 12 * i, 0, 12 * i)); //use a switch() to change set of colours
   }
   FastLED.show();
 
